97d2c4e93e4a1d0a3eace4f1504b8792bc9c186f
Remco <dewilderemco13@gmail.com>
2019-03-03 20:35:59 +0100

    init

---
+++ WebServerMultiThreaded/Response.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace WebServerMultiThreaded
+{
+    internal class Response
+    {
+        private readonly byte[] data;
+        private readonly string status;
+        private readonly string type;
+
+        private Response(string status, string type, byte[] data)
+        {
+            this.status = status;
+            this.data = data;
+            this.type = type;
+        }
+
+        public static Response SendResponse(Request request)
+        {
+            if (request == null) return NullRequest();
+            if (request.Type == "GET")
+            {
+                Console.WriteLine(request.URL);
+                var file = Environment.CurrentDirectory + Server.HOST_DIR + request.URL;
+                var fileInfo = new FileInfo(file);
+                if (fileInfo.Exists) return ReturnAFile(fileInfo);
+            }
+
+            return NullRequest();
+        }
+
+        private static Response ReturnAFile(FileInfo file)
+        {
+            var fs = file.OpenRead();
+            var reader = new BinaryReader(fs);
+            var d = new byte[fs.Length];
+            reader.Read(d, 0, d.Length);
+            fs.Close();
+            return new Response("200 OK", "html/text", d);
+        }
+
+        public static Response NullRequest()
+        {
+            var file = Environment.CurrentDirectory + Server.ERR_DIR + "400.html";
+            var fi = new FileInfo(file);
+            var fs = fi.OpenRead();
+            var reader = new BinaryReader(fs);
+            var d = new byte[fs.Length];
+            reader.Read(d, 0, d.Length);
+            fs.Close();
+            return new Response("400 Bad Request", "html/text", d);
+        }
+
+        public void Post(NetworkStream stream)
+        {
+            var sbHeader = new StringBuilder();
+            sbHeader.AppendLine(Server.VERSION + " " + status);
+            // CONTENT-LENGTH
+            sbHeader.AppendLine("Content-Length: " + data.Length);
+
+            // Append one more line breaks to seperate header and content.
+            sbHeader.AppendLine();
+            var response = new List<byte>();
+            // response.AddRange(bHeadersString);
+            response.AddRange(Encoding.ASCII.GetBytes(sbHeader.ToString()));
+            response.AddRange(data);
+            var responseByte = response.ToArray();
+            stream.Write(responseByte, 0, responseByte.Length);
+        }
+    }
+}
---
+++ WebServerMultiThreaded/Server.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebServerMultiThreaded
+{
+    internal class Server
+    {
+        private TcpListener _listener;
+        public string Localhost = "127.0.0.1";
+        public const string HOST_DIR = "/root/host";
+        public const string ERR_DIR = "/root/err/";
+        public const string VERSION = "HTTP/1.1";
+
+
+        public void StartServer()
+        {
+            try
+            {
+                const int port = 3000;
+                IPAddress localAddr = IPAddress.Parse(Localhost);
+                _listener = new TcpListener(localAddr, port);
+                _listener.Start();
+
+                string data = null;
+
+                while (true)
+                {
+                    //var client =  _listener.AcceptTcpClient();
+                    Task<TcpClient> client = _listener.AcceptTcpClientAsync();
+                    var thread = new Thread(() => HandleClient(client.Result));
+                    thread.Start();
+                    thread.Join();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public void HandleClient(TcpClient client)
+        {
+            try
+            {
+                var streamReader = new StreamReader(client.GetStream());
+
+                Request request = Request.DeConstructStreamReaderToString(streamReader);
+                Response response = Response.SendResponse(request);
+                response.Post(client.GetStream());
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            client.Close();
+        }
+
+    }
+}
